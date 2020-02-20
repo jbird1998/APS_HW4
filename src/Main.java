6ee19f5ac6b75d7b24c48ca0df40bc727e44dcaf
jbird1998 <31713500+jbird1998@users.noreply.github.com>
2020-02-19 23:20:31 -0500

    Unfinished line up v1

--- src/Main.java
+++ src/Main.java
@@ -1,76 +1,91 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        //TODO: Optimize (too many timeouts) and investigate wrong answers
-        //NOTE: Don't need to increment by T, wait times can be shorter.
+    public static void main (String[] args) {
         Scanner scan = new Scanner(System.in);
-        String line = scan.nextLine();
-        String[] lparts = line.split(" ");
+        int n = scan.nextInt();
+        String skip = scan.nextLine();
 
-        int N = Integer.parseInt(lparts[0]);
-        int T = Integer.parseInt(lparts[1]);
-        int M = Integer.parseInt(lparts[2]);
-
-        int[] arrivals = new int[M];
-        char[] directions = new char[M];
-
-        for (int i = 0; i < M; i++) {
+        HashMap<Integer, Integer> union = new HashMap<>();
+        ArrayList<ArrayList<Integer>> queue = new ArrayList<>();
+        ArrayList<Integer> output = new ArrayList<>();
+        String line;
+        String[] lparts;
+        int index = -1;
+        int totalListsPopped = 0;
+        for (int i = 0; i < n; i++) {
+            //TODO: Scan and parse groups
             line = scan.nextLine();
             lparts = line.split(" ");
-            arrivals[i] = Integer.parseInt(lparts[0]);
-            directions[i] = lparts[1].charAt(0);
+            int k = Integer.parseInt(lparts[0]);
+            int first = Integer.parseInt(lparts[1]);
+            union.put(first, first);
+            int key;
+            for (int j = 2; j <= k; j++) {
+                key = Integer.parseInt(lparts[j]);
+                union.put(key, first);
+            }
         }
-
-        int[] times = new int[M];
-        boolean[] visited = new boolean[M];
-        int index = 0;
-        char currentDir = 'l';
-        int time = 0;
-        int tripCount;
-        boolean changeDir;
-        boolean earliestNotVisitedSet = false;
-        int earliestNotVisited = 0;
-        while (index < M) {
-            //System.out.println(earliestNotVisited + " t: " + time);
-            tripCount = 0;
-            changeDir = false;
-            for (int i = earliestNotVisited; i < M; i++) {
-                if (!visited[i] && arrivals[i] <= time) {
-                    changeDir = true;
-                    if (directions[i] == currentDir && tripCount < N) {
-                        visited[i] = true;
-                        //System.out.println("VISITED");
-                        times[i] = time + T;
-                        tripCount++;
-                        index++;
+        line = scan.nextLine();
+        while (line.charAt(0) != 'S') {
+            if (line.charAt(1) == 'u') { // push operation
+                lparts = line.split(" ");
+                int key = Integer.parseInt(lparts[1]);
+                try {
+                    int found = find(union, key);
+                    int place = union.get(found);
+                    if (place > 0) { // this indicates we haven't yet added an element of this group
+                        union.replace(found, index);
+                        index--;
+                        ArrayList<Integer> node = new ArrayList<>();
+                        node.add(key);
+                        queue.add(node);
+                    } else {
+                        place = -1 * place - 1; // the index of the group
+                        ArrayList<Integer> node = queue.get(place - totalListsPopped);
+                        node.add(key);
+                    }
+                } catch (NullPointerException e) {
+                    ArrayList<Integer> node = new ArrayList<>();
+                    node.add(key);
+                    queue.add(node);                            
+                }
+            } else { // pop operation
+                if (queue.size() != 0) {
+                    ArrayList<Integer> node = queue.get(0);
+                    int num = node.get(0);
+                    output.add(num);
+                    if (node.size() > 1) {
+                        node.remove(0);
+                    } else { // last node present in group
+                        try {
+                            int found = find(union, num);
+                            union.replace(found, found);
+                        } catch (NullPointerException e) {
+                            // uh yeah, catch.
+                        }
+                        queue.remove(0);
+                        totalListsPopped++;
                     }
                 }
-                if (!visited[i] && !earliestNotVisitedSet) {
-                    earliestNotVisitedSet = true;
-                    earliestNotVisited = i;
-                }
-               //System.out.println("\t v: " + visited[i]);
             }
-            if (changeDir) {
-                if (currentDir == 'l') {
-                    currentDir = 'r';
-                } else {
-                    currentDir = 'l';
-                }
-                time += T;
-            } else {
-//                if (time - arrivals[earliestNotVisited] < T && 0 < time - arrivals[earliestNotVisited]) {
-//                    time = arrivals[earliestNotVisited];
-//                } else {
-                    time++;
-                //
-            }
-            earliestNotVisitedSet = false;
+            line = scan.nextLine();
         }
-        for (int a: times) {
-            System.out.println(a);
+        for (int o: output) {
+            System.out.println(o);
         }
+    }
+
+    public static int find(HashMap<Integer, Integer> map, int k) {
+        int v = map.get(k);
+        if (k == v || v < 0) {
+            return k;
+        }
+        int x = find(map, v);
+        map.replace(k, x);
+        return x;
     }
 }
---
+++ src/ferry.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class ferry {
+
+    public static void main(String[] args) {
+        //TODO: Optimize (too many timeouts) and investigate wrong answers
+        //NOTE: Don't need to increment by T, wait times can be shorter.
+        Scanner scan = new Scanner(System.in);
+        String line = scan.nextLine();
+        String[] lparts = line.split(" ");
+
+        int N = Integer.parseInt(lparts[0]);
+        int T = Integer.parseInt(lparts[1]);
+        int M = Integer.parseInt(lparts[2]);
+
+        int[] arrivals = new int[M];
+        char[] directions = new char[M];
+
+        for (int i = 0; i < M; i++) {
+            line = scan.nextLine();
+            lparts = line.split(" ");
+            arrivals[i] = Integer.parseInt(lparts[0]);
+            directions[i] = lparts[1].charAt(0);
+        }
+
+        int[] times = new int[M];
+        boolean[] visited = new boolean[M];
+        int index = 0;
+        char currentDir = 'l';
+        int time = 0;
+        int tripCount;
+        boolean changeDir;
+        boolean earliestNotVisitedSet = false;
+        int earliestNotVisited = 0;
+        while (index < M) {
+            //System.out.println(earliestNotVisited + " t: " + time);
+            tripCount = 0;
+            changeDir = false;
+            for (int i = earliestNotVisited; i < M; i++) {
+                if (!visited[i] && arrivals[i] <= time) {
+                    changeDir = true;
+                    if (directions[i] == currentDir && tripCount < N) {
+                        visited[i] = true;
+                        //System.out.println("VISITED");
+                        times[i] = time + T;
+                        tripCount++;
+                        index++;
+                    }
+                }
+                if (!visited[i] && !earliestNotVisitedSet) {
+                    earliestNotVisitedSet = true;
+                    earliestNotVisited = i;
+                }
+               //System.out.println("\t v: " + visited[i]);
+            }
+            if (changeDir) {
+                if (currentDir == 'l') {
+                    currentDir = 'r';
+                } else {
+                    currentDir = 'l';
+                }
+                time += T;
+            } else {
+//                if (time - arrivals[earliestNotVisited] < T && 0 < time - arrivals[earliestNotVisited]) {
+//                    time = arrivals[earliestNotVisited];
+//                } else {
+                    time++;
+                //
+            }
+            earliestNotVisitedSet = false;
+        }
+        for (int a: times) {
+            System.out.println(a);
+        }
+    }
+}
